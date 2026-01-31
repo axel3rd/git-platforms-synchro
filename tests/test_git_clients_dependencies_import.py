022ea40e04237e5a3e45d0613b73e0eb903ddf32
Alix Lourme <alix.lourme@gmail.com>
2026-01-31 02:48:11 +0100

    Try to improve

---
+++ tests/test_git_clients_dependencies_import.py
@@ -0,0 +1,27 @@
+import re
+from pytest import raises, skip
+
+# This test mock 'sys.modules' which is shared between all tests
+# So if executed alone, it work, but not in all tests suite
+# A solution should be found to isolate it :-(
+
+@skip(reason='Should be isolated')
+def test_no_github(mocker):
+    mocker.patch.dict('sys.modules', {'github': None})
+    with raises(ValueError, match=re.escape('Type "github" not supported or not detected from URL "http://localhost". Or python client dependency not installed - GitHub (PyGithub): False, Gitea (py-gitea): True, Bitbucket (atlassian-python-api): True')):
+        from modules.git_clients import GitClientFactory
+        GitClientFactory.create_client('http://localhost', 'github')
+
+@skip(reason='Should be isolated')
+def test_no_gitea(mocker):
+    mocker.patch.dict('sys.modules', {'gitea': None})
+    with raises(ValueError, match=re.escape('Type "gitea" not supported or not detected from URL "http://localhost". Or python client dependency not installed - GitHub (PyGithub): True, Gitea (py-gitea): False, Bitbucket (atlassian-python-api): True')):
+        from modules.git_clients import GitClientFactory
+        GitClientFactory.create_client('http://localhost', 'gitea')
+
+@skip(reason='Should be isolated')
+def test_no_bitbucket(mocker):
+    mocker.patch.dict('sys.modules', {'atlassian': None})
+    with raises(ValueError, match=re.escape('Type "bitbucket" not supported or not detected from URL "http://localhost". Or python client dependency not installed - GitHub (PyGithub): True, Gitea (py-gitea): True, Bitbucket (atlassian-python-api): False')):
+        from modules.git_clients import GitClientFactory
+        GitClientFactory.create_client('http://localhost', 'bitbucket')     
