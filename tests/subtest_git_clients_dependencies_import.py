dfeb03ae82b4e459d9fbaca15b779643b34f8403
Alix Lourme <alix.lourme@gmail.com>
2026-01-31 03:46:02 +0100

    complement

--- tests/subtest_git_clients_dependencies_import.py
+++ tests/subtest_git_clients_dependencies_import.py
@@ -6,20 +6,23 @@ from contextlib import contextmanager
 # So if executed alone, it work, but not in all tests suite
 # So complete isolation for each test with a new context
 
+STR_SYS_MODULES = 'sys.modules'
+STR_LOCALHOST = 'http://localhost'
+
 def test_no_github(mocker):
-    mocker.patch.dict('sys.modules', {'github': None})
+    mocker.patch.dict(STR_SYS_MODULES, {'github': None})
     with raises(ValueError, match=re.escape('Type "github" not supported or not detected from URL "http://localhost". Or python client dependency not installed - GitHub (PyGithub): False, Gitea (py-gitea): True, Bitbucket (atlassian-python-api): True')):
         from modules.git_clients import GitClientFactory
-        GitClientFactory.create_client('http://localhost', 'github')
+        GitClientFactory.create_client(STR_LOCALHOST, 'github')
 
 def test_no_gitea(mocker):
-    mocker.patch.dict('sys.modules', {'gitea': None})
+    mocker.patch.dict(STR_SYS_MODULES, {'gitea': None})
     with raises(ValueError, match=re.escape('Type "gitea" not supported or not detected from URL "http://localhost". Or python client dependency not installed - GitHub (PyGithub): True, Gitea (py-gitea): False, Bitbucket (atlassian-python-api): True')):
         from modules.git_clients import GitClientFactory
-        GitClientFactory.create_client('http://localhost', 'gitea')
+        GitClientFactory.create_client(STR_LOCALHOST, 'gitea')
 
 def test_no_bitbucket(mocker):
-    mocker.patch.dict('sys.modules', {'atlassian': None})
+    mocker.patch.dict(STR_SYS_MODULES, {'atlassian': None})
     with raises(ValueError, match=re.escape('Type "bitbucket" not supported or not detected from URL "http://localhost". Or python client dependency not installed - GitHub (PyGithub): True, Gitea (py-gitea): True, Bitbucket (atlassian-python-api): False')):
         from modules.git_clients import GitClientFactory
-        GitClientFactory.create_client('http://localhost', 'bitbucket')     
+        GitClientFactory.create_client(STR_LOCALHOST, 'bitbucket')     
