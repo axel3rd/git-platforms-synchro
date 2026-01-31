64fc52551cd0a4901eee426f976b8e0f77e0e205
Alix Lourme <alix.lourme@gmail.com>
2026-01-31 03:39:40 +0100

    Remove Python 3.9 and 3.10

--- tests/test_git_clients_dependencies_import.py
+++ tests/test_git_clients_dependencies_import.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 
+# File should be completly isolated, so dedicated process (works only from Python 3.11 & Pytest)
 def test_sub():
     test_file = "tests/subtest_git_clients_dependencies_import.py"
     subprocess.run([sys.executable, "-m", "pytest", test_file], check=True)
