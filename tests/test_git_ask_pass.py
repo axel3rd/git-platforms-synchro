02f1587f44d30f03eb9478b420eb2986b92e2358
Alix Lourme <alix.lourme@gmail.com>
2026-07-26 00:39:40 +0200

    Improve SonarQube analyse (#37)

--- tests/test_git_ask_pass.py
+++ tests/test_git_ask_pass.py
@@ -1,6 +1,5 @@
 import os
 from modules.git_askpass import read_input_from_sys_env
-from pytest import LogCaptureFixture
 
 
 def test_bad_input():
@@ -14,21 +13,21 @@ def test_no_sys_env(capsys):
     assert capsys.readouterr().out == '\n\n'
 
 
-def test_username(capsys):
-    os.environ['GIT_USERNAME'] = 'foo'
+def test_username(capsys, monkeypatch):
+    monkeypatch.setenv('GIT_USERNAME', 'foo')
     assert read_input_from_sys_env('Username') == 0
     assert capsys.readouterr().out == 'foo\n'
 
 
-def test_password(capsys):
-    os.environ['GIT_PASSWORD'] = 'bar'  # noqa
+def test_password(capsys, monkeypatch):
+    monkeypatch.setenv('GIT_PASSWORD', 'bar')  # noqa
     assert read_input_from_sys_env('Password') == 0
     assert capsys.readouterr().out == 'bar\n'
 
 
-def test_username_and_password(capsys):
-    os.environ['GIT_USERNAME'] = 'foo'
-    os.environ['GIT_PASSWORD'] = 'bar'  # noqa
+def test_username_and_password(capsys, monkeypatch):
+    monkeypatch.setenv('GIT_USERNAME', 'foo')
+    monkeypatch.setenv('GIT_PASSWORD', 'bar')  # noqa
     assert read_input_from_sys_env('username') == 0
     assert read_input_from_sys_env('password') == 0
     assert capsys.readouterr().out == 'foo\nbar\n'
